9590f38016350cbc43bb1ce62a998ec2097265f7
Saevus96 <kp.chlewski@gmail.com>
2019-01-23 13:50:11 +0100

    Nueron Hebba skończony

--- neuron hebba/main.cpp
+++ neuron hebba/main.cpp
@@ -3,19 +3,19 @@
 
 double f(double s)
 {
-	double beta = 4.0;
+	double beta = 0.5;
 	return(1.0 - exp(-beta * s)) / (1.0 + exp(-beta * s));
 
 }
 int main() {
 
 	//wartosci
-	double tab1[13] = { 1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0 ,-1.0,-1.0,1.0 }; //jedynka	
-	double tab4[13] = { 1.0,1.0,-1.0,1.0,1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0 }; //czworka
-
+	double tab1[13] = { -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0 }; //jedynka	
+	double tab4[13] = { 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0 }; //czworka
+	
 	//wagi	
 	double wx1[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
-	double wx2[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
+	double wx4[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
 	
 	//wartosci oczekiwane
 	double d1 = 1.0;
@@ -27,51 +27,46 @@ int main() {
 	double E4;
 	
 	//wspolczynnik uczenia
-	double ni = 0.4;
+	double ni = 0.5;
 	
 	//wartosc funckji
 	double y1;
 	double y4;
 
 	//suma
-	double sum;
+	double suma1 = 0.0;
+	double suma4 = 0.0;
 	
 	
 	int d = 0; //kroki 	
 	
 	do {
-		//dla jedynki 
-		sum = 0;
+		E = 0;
 		for (int i = 0; i < 13; i++)
 		{
-			sum = sum + tab1[i] * wx1[i];
-			y1 = f(sum);	
-			wx1[i] = wx1[i] + ni * d1*tab1[i];
-			E1 = (0.5)*((d1 - y1)*(d1 - y1));
-			d++;
+			suma1 += tab1[i] * wx1[i];
+			suma4 += tab4[i] * wx4[i];
 		}
-		
-		//dla czworki
-		sum = 0;
-		for (int i = 0; i < 13; i++)
+
+		y1 = f(suma1);						
+		y4 = f(suma4);
+
+		for (int i = 0; i < 13; i++)		
 		{
-			sum = sum + tab4[i] * wx2[i];
-			y4 = f(sum);
-			wx2[i] = wx2[i] + ni * d4*tab4[i];
-			E4 = (0.5)*((d4 - y4)*(d4 - y4));
-			d++;
-		}
+			wx1[i] = wx1[i] + ni * d1 * tab1[i];
+			wx4[i] = wx4[i] + ni * d4 * tab4[i];
+		}	
+
+		E1 = 0.5 * (d1 - y1)*(d1 - y1);				
+		E4 = 0.5 * (d4 - y4)*(d4 - y4);
 		
 		d++; //krok programu
-		E = E1 + E4;
-		if (E < 0.03) {
-			std::cout <<"Wynik dla jeden: "<< y1 << "  Wynik dla cztery: " << y4 << std::endl;
-			std::cout << "Krok programu: " << d << std::endl;
-		}
+		E = E1 + E4;		
 		
-	} while (E > 0.03);
+	} while (E > 0.0003);
 	
-
+	std::cout << "Wynik dla jeden: " << y1 << "  Wynik dla cztery: " << y4 << std::endl;
+	std::cout << "Krok programu: " << d << std::endl;
 
 	system("pause");
 	return 0;
