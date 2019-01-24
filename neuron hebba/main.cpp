937b07c81dc12281b315d3b6c075d58cb0f2f310
Saevus96 <kp.chlewski@gmail.com>
2019-01-24 18:30:00 +0100

    ostateczny hebba

--- neuron hebba/main.cpp
+++ neuron hebba/main.cpp
@@ -1,75 +1,45 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 #include <cmath>
+#include <math.h>
 
-double f(double s)
-{
-	double beta = 0.5;
-	return(1.0 - exp(-beta * s)) / (1.0 + exp(-beta * s));
+double beta = 0.4;
 
+double f(double tabx) {
+	return (1.0 - exp(-beta * tabx)) / (1.0 + exp(-beta * tabx));
 }
+
 int main() {
-
-	//wartosci
-	double tab1[13] = { -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0 }; //jedynka	
-	double tab4[13] = { 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0 }; //czworka
-	
-	//wagi	
-	double wx1[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
-	double wx4[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
-	
-	//wartosci oczekiwane
-	double d1 = 1.0;
-	double d4 = -1.0;
-
-	//bledy
+	double tabx[2][13] = { { 1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0 },
+						   { 1.0,1.0,-1.0,1.0,1.0,1.0,1.0,-1.0,-1.0,1.0,-1.0,-1.0,1.0 } };
+	double d[2] = { 1.0, -1.0 };
+	double wx[13] = { 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 };
+	double y[2];
+	double n = 0.4;
 	double E;
-	double E1;
-	double E4;
-	
-	//wspolczynnik uczenia
-	double ni = 0.5;
-	
-	//wartosc funckji
-	double y1;
-	double y4;
+	double sum;
 
-	//suma
-	double suma1 = 0.0;
-	double suma4 = 0.0;
-	
-	
-	int d = 0; //kroki 	
-	
 	do {
-		E = 0;
-		for (int i = 0; i < 13; i++)
-		{
-			suma1 += tab1[i] * wx1[i];
-			suma4 += tab4[i] * wx4[i];
+		E = 0.0;
+		for (int i = 0; i < 2; i++) {
+			sum = 0.0;
+			for (int j = 0; j < 13; j++) {
+				sum += tabx[i][j] * wx[j];
+			}
+
+			y[i] = f(sum);
+
+			for (int j = 0; j < 13; j++) {
+				wx[j] = wx[j] + n * d[i] * tabx[i][j];
+			}
+
+			E += 0.5*(d[i] - y[i])*(d[i] - y[i]);
 		}
 
-		y1 = f(suma1);						
-		y4 = f(suma4);
-
-		for (int i = 0; i < 13; i++)		
-		{
-			wx1[i] = wx1[i] + ni * d1 * tab1[i];
-			wx4[i] = wx4[i] + ni * d4 * tab4[i];
-		}	
-
-		E1 = 0.5 * (d1 - y1)*(d1 - y1);				
-		E4 = 0.5 * (d4 - y4)*(d4 - y4);
-		
-		d++; //krok programu
-		E = E1 + E4;		
-		
 	} while (E > 0.0003);
-	
-	std::cout << "Wynik dla jeden: " << y1 << "  Wynik dla cztery: " << y4 << std::endl;
-	std::cout << "Krok programu: " << d << std::endl;
+
+	std::cout << y[0] << " " << y[1] << std::endl;
 
 	system("pause");
 	return 0;
-	
-	
 }
